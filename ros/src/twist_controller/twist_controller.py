702687f03acf479765af55123a35942c550dc51e
ninginthecloud <liningblack@hotmail.com>
2018-07-08 00:59:59 -0700

    update steer pid for smoonthly turning

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -7,6 +7,14 @@ import rospy
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
+# parameter tunning- throttle PID
+Kp_v = 0.3#0.3
+Ki_v = 0.1#0.1
+Kd_v = 0.01#0.0
+# parameter tunning- steering PID
+Kp_s = 0.15#0.15
+Ki_s = 0.001
+Kd_s = 0.1#0.1
 
 class Controller(object):
     def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit, accel_limit, 
@@ -15,12 +23,15 @@ class Controller(object):
         self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
         
         min_throttle = 0.0
-        max_throttle = 0.9
-        self.throttle_controller = PID(kp=0.3, ki=0.1, kd=0.0,
+        max_throttle = 0.7
+        self.throttle_controller = PID(kp=Kp_v, ki=Ki_v, kd=Kd_v,
                                         mn=min_throttle,
                                         mx=max_throttle)
+        self.steering_controller = PID(kp=Kp_s, ki=Ki_s, kd=Kd_s,
+                                        mn=-1.0*max_steer_angle,
+                                        mx=1.0*max_steer_angle) 
 
-        self.vel_lpf = LowPassFilter(tau=0.5, ts=0.02)
+        self.vel_lpf = LowPassFilter(tau=0.6, ts=0.02)
 
         self.vehicle_mass = vehicle_mass
         self.fuel_capacity = fuel_capacity
@@ -31,16 +42,17 @@ class Controller(object):
 
         self.last_time = rospy.get_time()
 
-    def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
+    def control(self, current_vel, dbw_enabled, linear_vel, angular_vel, cte):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
         if not dbw_enabled:
             self.throttle_controller.reset()
+            self.steering_controller.reset()
             return 0.0, 0.0, 0.0
 
-        current_vel = self.vel_lpf.filt(current_vel)
+        #current_vel = self.vel_lpf.filt(current_vel)
 
-        steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+        #throttle
         vel_error = linear_vel - current_vel
         self.last_vel = current_vel
         current_time = rospy.get_time()
@@ -48,8 +60,9 @@ class Controller(object):
         self.last_time = current_time
 
         throttle = self.throttle_controller.step(vel_error, sample_time)
+        
+        #brake
         brake = 0 
-
         if linear_vel==0. and current_vel<0.1:
             throttle = 0
             brake = 700
@@ -57,5 +70,11 @@ class Controller(object):
             throttle = 0
             decel = max(vel_error, self.decel_limit)
             brake = abs(decel)*self.vehicle_mass*self.wheel_radius
-            
-        return throttle, brake, steering
+
+        #steering
+        predict_steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+        steering_err = self.steering_controller.step(cte, sample_time)
+        steering = predict_steering + steering_err
+
+        #return throttle, brake, predict_steering
+        return throttle, brake, steering_err
