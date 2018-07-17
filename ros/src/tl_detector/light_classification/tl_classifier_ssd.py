aba22d35a8c6c9c1bc45d6fe1d8fe50642d932fb
DhruvaKumar <dhruva.kumar08@gmail.com>
2018-07-17 11:46:19 -0400

    feat: making inference faster by loading sess and tensors once

--- ros/src/tl_detector/light_classification/tl_classifier_ssd.py
+++ ros/src/tl_detector/light_classification/tl_classifier_ssd.py
@@ -2,8 +2,6 @@ from styx_msgs.msg import TrafficLight
 import rospy
 import numpy as np
 import os
-import sys
-from glob import glob
 import tensorflow as tf
 from time import time
 
@@ -21,10 +19,20 @@ class TLClassifier(object):
     def __init__(self):
         
         # load frozen graph
-        self.graph = self.load_graph()
-        rospy.logdebug('TLClassifier: loaded model')
+        graph = self.load_graph()
+
+        # load tf session
+        self.sess = tf.Session(graph=graph)
+
+        # get tensors
+        self.image_tensor = graph.get_tensor_by_name('image_tensor:0')
+        self.num_detections_tnsr = graph.get_tensor_by_name('num_detections:0')
+        self.classes_tnsr = graph.get_tensor_by_name('detection_classes:0')
+        # self.boxes_tnsr = graph.get_tensor_by_name('detection_boxes:0')
+        self.scores_tnsr = graph.get_tensor_by_name('detection_scores:0')
+
+        rospy.loginfo('tl_classifier: loaded model and sess')
         
-        self.count_save = 0
 
     def load_graph(self):
         '''
@@ -54,51 +62,36 @@ class TLClassifier(object):
 
         state = TrafficLight.UNKNOWN
 
-        with tf.Session(graph=self.graph) as sess:
-            
-            # get tensors
-            num_detections_tnsr = tf.get_default_graph().get_tensor_by_name('num_detections:0')
-            classes_tnsr = tf.get_default_graph().get_tensor_by_name('detection_classes:0')
-#             boxes_tnsr = tf.get_default_graph().get_tensor_by_name('detection_boxes:0')
-            scores_tnsr = tf.get_default_graph().get_tensor_by_name('detection_scores:0')
+        # run inference
+        [num_detections, classes, scores] = self.sess.run([
+            self.num_detections_tnsr, self.classes_tnsr, self.scores_tnsr],
+            feed_dict={self.image_tensor: image[None,:]})
         
-            image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+        num_detections =  int(num_detections)
+        classes = classes[0].astype(np.uint8)
+        # boxes = boxes[0]
+        scores = scores[0]
+
+        # max score
+        max_score_id = np.argmax(scores)
+        max_score = scores[max_score_id]
+        class_max_score = classes[max_score_id]
         
-            # run inference
-            [num_detections, classes, scores] = sess.run([
-                num_detections_tnsr, classes_tnsr, scores_tnsr],
-                feed_dict={image_tensor: image[None,:]})
-            
-            num_detections =  int(num_detections)
-            classes = classes[0].astype(np.uint8)
-#             boxes = boxes[0]
-            scores = scores[0]
+        # return class of max score
+        if (num_detections > 0 and max_score > SCORE_THRESHOLD):
+            if (class_max_score == 1):
+                state = TrafficLight.RED
+            elif (class_max_score == 2):
+                state = TrafficLight.YELLOW
+            elif (class_max_score == 3):
+                state = TrafficLight.GREEN
 
-            # max score
-            max_score_id = np.argmax(scores)
-            max_score = scores[max_score_id]
-            class_max_score = classes[max_score_id]
-            
-            if (num_detections > 0 and max_score > SCORE_THRESHOLD):
-
-                # return class of max score
-                if (class_max_score == 1):
-                    state = TrafficLight.RED
-                elif (class_max_score == 2):
-                    state = TrafficLight.YELLOW
-                elif (class_max_score == 3):
-                    state = TrafficLight.GREEN
-
-            rospy.logdebug('tl_classifier: detection took {:.3f}s state:{} tstate:{}'.format(time() - start, state, true_state))
-            
-            
-            # debug misclassification
-            if state != true_state:
-                cv2.imwrite('misclassifications/{}_{}_{}_{:.2f}.png'.format(state, true_state, class_max_score, max_score), cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
-                print('tl_classifier: saving misclassified image...')
-#             if (self.count_save < 2):
-#                 cv2.imwrite('{}_{}.png'.format(self.count_save, state), cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
-#                 print(output_dict)
-#                 self.count_save += 1
+        rospy.logdebug('tl_classifier: detection took {:.3f}s state:{} tstate:{}'.format(time() - start, state, true_state))
+        
+        
+        # debug misclassification
+        if state != true_state:
+            cv2.imwrite('misclassifications/{}_{}_{}_{:.2f}.png'.format(state, true_state, class_max_score, max_score), cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+            rospy.logdebug('tl_classifier: saving misclassified image...')
 
         return state
